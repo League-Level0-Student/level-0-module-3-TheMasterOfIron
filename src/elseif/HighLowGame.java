834175ec8bd250eba3ecfb2242d2f67161500717
league <league@wts22.attlocal.net>
2017-12-03 11:27:28 -0800

    Doesn't work YET

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,41 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(99 + 1);
 		// 2. Print out the random variable above
-		
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+		for (int i = 0; i < 10; i++) {
+			// 1. ask the user for a guess using a pop-up window, and save their response
+			String inserted = JOptionPane.showInputDialog("What is your guess.");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int guess = Integer.parseInt(inserted);
 			// 5. if the guess is correct
+			if (guess == random) {
 				// 6. win
+				JOptionPane.showMessageDialog(null, "You guessed right!!!");
+				JOptionPane.showMessageDialog(null, "You win!!!");
+				break;
+			}
 			// 7. if the guess is high
+			else if (guess > random) {
 				// 8. tell them it's too high
+				JOptionPane.showMessageDialog(null, "You guessed too high try again next time.");
+				JOptionPane.showMessageDialog(null, "You lose.");
+			}
 			// 9. if the guess is low
+			else if (guess < random) {
 				// 10. tell them it's too low
-
-		// 12. tell them they lose
+				JOptionPane.showMessageDialog(null, "You guessed too low try again next time.");
+				JOptionPane.showMessageDialog(null, "You lose.");
+			}
+			// 12. tell them they lose
+		}
 	}
 
 }
-
-
--- src/methods/HappyPet.java
+++ src/methods/HappyPet.java
@@ -7,27 +7,40 @@ import javax.swing.JOptionPane;
 
 public class HappyPet {
 	// 2. Create a happinessLevel variable to store the pet's happiness number.
-	//    Initialize to zero.
+	// Initialize to zero.
+	static int happinessLevel = 0;
 
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store in variable
-
+		String pet = JOptionPane.showInputDialog("What pet do you want to buy.");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
 
-			// 5. Use user input to call the appropriate method created in step 4.
+		// 3. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+		int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet " + pet + " happy?",
+				"Happy pet.", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Feed it.", "Give it water.",
+						"Groom.", "Cuddle.", "Take it for a walk.", "Clean up its poop." },
+				null);
 
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+		// 5. Use user input to call the appropriate method created in step 4.
+		if (task == 6) {
+			feedIt();
+		} else {
+			System.out.println("Doesnt work.");
+		}
+		// 6. If you determine the happiness level is large enough, tell the
+		// user that he loves his pet and use break; to exit for loop.
 
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+	static public void feedIt() {
+		JOptionPane.showMessageDialog(null, "You feed your pet and it becomes happier.");
+		happinessLevel += 1;
+	}
 }
